f44ca19976177dbc11b1032ef0d27a1b40893c4e
Kunwar Luthera <kunwarluthera@Kunwars-MBP.frontierlocal.net>
2019-03-04 05:28:05 -0600

    updated app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Deployed application on docker using docker compose
+            Deployed application on feature branch
           </p>
           <a
             className="App-link"
